4df5d123e474794b3a2a1617750fd4607fd03419
Mathias Stemen <mstemen@gmail.com>
2023-06-01 13:17:35 -0500

    fleshed out map class

--- src/main/java/wms/EPubToMusic/WordOccurMatrix.java
+++ src/main/java/wms/EPubToMusic/WordOccurMatrix.java
@@ -32,61 +32,62 @@ public class WordOccurMatrix implements Map<String, Integer> { PageStack assocPa
 
     @Override
     public int size() {
-        return 0;
+        return wordOccurMatrix.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return wordOccurMatrix.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        return wordOccurMatrix.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        return wordOccurMatrix.containsValue(value);
     }
 
     @Override
     public Integer get(Object key) {
-        return null;
+        return wordOccurMatrix.get(key);
     }
 
     @Override
     public Integer put(String key, Integer value) {
-        return null;
+        return wordOccurMatrix.put(key, value);
     }
 
     @Override
     public Integer remove(Object key) {
-        return null;
+        return wordOccurMatrix.remove(key);
     }
 
     @Override
     public void putAll(Map<? extends String, ? extends Integer> m) {
-
+        wordOccurMatrix.putAll(m);
     }
 
     @Override
     public void clear() {
+        wordOccurMatrix.clear();
 
     }
 
     @Override
     public Set<String> keySet() {
-        return null;
+        return wordOccurMatrix.keySet();
     }
 
     @Override
     public Collection<Integer> values() {
-        return null;
+        return wordOccurMatrix.values();
     }
 
     @Override
     public Set<Entry<String, Integer>> entrySet() {
-        return null;
+        return wordOccurMatrix.entrySet();
     }
 }
